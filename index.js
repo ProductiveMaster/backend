f5222f668b8fb0b466e2ea59c9ec92f9b7856a19
Emiliano Pamont <emipmttt@gmail.com>
2020-12-18 20:47:12 -0600

    .

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 
 const config = require('./config/index');
 const db = require('./lib/db');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+app.use(express.urlencoded({
+    extended: false
+}));
 
-app.get('/api', (req, res) => { res.send('Hello World') });
+app.get('/api', (req, res) => {
+    res.send('Hello World')
+});
 
 //Database Connect
 db.connect();
